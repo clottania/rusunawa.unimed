2d2fbda7a207d95c39705ec14aed7fd134927e0f
Catarina Tania Anabela <143130171+clottania@users.noreply.github.com>
2025-04-19 19:35:09 +0700

    Add files via upload

---
+++ news/config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://rusunawa.unimed.ac.id/news/',
+	'parameter' => 'id',
+	'template'  => 'temp11.html',
+	'keywords'  => 'kawesatu.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
